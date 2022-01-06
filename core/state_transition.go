2595ce21b2a7652c3b5787c964610f676708bab7
Andrei Maiboroda <andrei@ethereum.org>
2022-01-06 18:17:21 +0100

    core: Make creation transaction invalid if initcode size above limit

--- core/state_transition.go
+++ core/state_transition.go
@@ -336,6 +336,11 @@ func (st *StateTransition) TransitionDb() (*ExecutionResult, error) {
 		return nil, fmt.Errorf("%w: address %v", ErrInsufficientFundsForTransfer, msg.From().Hex())
 	}
 
+	// Check whether the init code size has been exceeded.
+	if eip3860 && contractCreation && len(st.data) > params.MaxInitCodeSize {
+		return nil, fmt.Errorf("%w: code size %v limit %v", ErrMaxInitCodeSizeExceeded, len(st.data), params.MaxInitCodeSize)
+	}
+
 	// Set up the initial access list.
 	if rules.IsBerlin {
 		st.state.PrepareAccessList(msg.From(), msg.To(), vm.ActivePrecompiles(rules), msg.AccessList())
